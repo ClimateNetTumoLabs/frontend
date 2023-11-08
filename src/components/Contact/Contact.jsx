3bc0e0cf2104fbfc1f808c8c5f7d14c34c3f8db0
eriktumosaryan <erik.saryan@tumo.org>
2023-11-08 17:01:01 +0400

    fix contact us page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,101 +1,92 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 import styles from "./Contact.module.css";
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    surname: "",
-    email: "",
-    message: "",
-  });
-
   const onSubmit = (e) => {
     e.preventDefault();
+    const formData = {
+      name: document.getElementById("name").value,
+      surname: document.getElementById("surname").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
 
     axios
-      .post("contact/", formData)
-      .then((response) => {
-        console.log("Form submitted successfully:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error submitting form:", error);
-      });
+        .post("contact/", formData)
+        .then((response) => {
+          console.log("Form submitted successfully:", response.data);
+        })
+        .catch((error) => {
+          console.error("Error submitting form:", error);
+        });
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-  console.log(formData);
   return (
-    <div className="container mt-5 mb-5 col-md-6 col-12">
-      <h2 className={`mb-3 ${styles.contact_us_title}`}> Contact Us</h2>
-      <form onSubmit={onSubmit}>
-        <div className={`d-flex flex-wrap`}>
-          <div
-            className={`col-12 mb-3 col-sm-6 ${styles.name_field} ${styles.contact_block}`}
-          >
-            <label className="form-label" htmlFor="name">
-              Name
+      <div className="container mt-5 mb-5 col-md-6 col-12">
+        <h2 className={`mb-3 ${styles.contact_us_title}`}> Contact Us</h2>
+        <form onSubmit={onSubmit}>
+          <div className={`d-flex flex-wrap`}>
+            <div
+                className={`col-12 mb-3 col-sm-6 ${styles.name_field} ${styles.contact_block}`}
+            >
+              <label className="form-label" htmlFor="name">
+                Name
+              </label>
+              <input
+                  className={`form-control ${styles.input_block}`}
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+              />
+            </div>
+            <div
+                className={`col-12 mb-3 col-sm-6 ${styles.surname_field} ${styles.contact_block}`}
+            >
+              <label className="form-label" htmlFor="surname">
+                Surname
+              </label>
+              <input
+                  className={`form-control ${styles.input_block}`}
+                  type="text"
+                  id="surname"
+                  name="surname"
+                  required
+              />
+            </div>
+          </div>
+          <div className={`mb-3 ${styles.contact_block}`}>
+            <label className="form-label" htmlFor="email">
+              Email
             </label>
             <input
-              className={`form-control ${styles.input_block}`}
-              type="text"
-              id="name"
-              name="name"
-              required
-              onChange={handleChange}
+                className={`form-control ${styles.input_block}`}
+                type="email"
+                id="email"
+                name="email"
+                required
             />
           </div>
-          <div
-            className={`col-12 mb-3 col-sm-6 ${styles.surname_field} ${styles.contact_block}`}
-          >
-            <label className="form-label" htmlFor="surname">
-              Surname
+          <div className={`mb-3 ${styles.contact_block}`}>
+            <label className="form-label" htmlFor="message">
+              Message
             </label>
-            <input
-              className={`form-control ${styles.input_block}`}
-              type="text"
-              id="surname"
-              name="surname"
-              required
-              onChange={handleChange}
+            <textarea
+                className={`form-control ${styles.input_block}`}
+                id="message"
+                name="message"
+                required
             />
           </div>
-        </div>
-        <div className={`mb-3 ${styles.contact_block}`}>
-          <label className="form-label" htmlFor="email">
-            Email
-          </label>
-          <input
-            className={`form-control ${styles.input_block}`}
-            type="email"
-            id="email"
-            name="email"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <div className={`mb-3 ${styles.contact_block}`}>
-          <label className="form-label" htmlFor="message">
-            Message
-          </label>
-          <textarea
-            className={`form-control ${styles.input_block}`}
-            id="message"
-            name="message"
-            required
-            onChange={handleChange}
-          />
-        </div>
-        <button
-          className={`mt-3 btn  ${styles.contact_us_button}`}
-          type="submit"
-        >
-          Send
-        </button>
-      </form>
-    </div>
+          <button
+              className={`mt-3 btn  ${styles.contact_us_button}`}
+              type="submit"
+          >
+            Send
+          </button>
+        </form>
+      </div>
   );
 };
 
