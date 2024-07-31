9e6338c7d1618522e59247f7583c4e7a8e43c7ee
Sona Arzumanyan <sona.arzumanyan42@gmail.com>
2024-08-01 00:43:03 +0400

    staging into main

--- src/i18n.js
+++ src/i18n.js
@@ -59,7 +59,7 @@ const resources = {
                     rain2: "Each tip of the bucket corresponds to a known quantity of rainfall, typically 0.2mm, which then generates a pulse signal used to calculate the total rainfall.",
                     rain: {
                         rainfall: "Rainfall",
-                        intensity: "Intensity (mm)",
+                        intensity: "Intensity (mm/hr)",
                         table: {
                             light: "Light",
                             moderate: "Moderate",
@@ -418,7 +418,7 @@ const resources = {
                     titleRain: "Անձրևի քանակ",
                     rain: {
                         rainfall: "Անձրև",
-                        intensity: "Ուժգնություն (մմ)",
+                        intensity: "Ուժգնություն (մմ/ժ)",
                         table: {
                             light: "Թեթև",
                             moderate: "Չափավոր",
