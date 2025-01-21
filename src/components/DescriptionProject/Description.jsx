7deaf60d2bb2c0fe1fb085aa7ef165ca36d2be80
eriktumosaryan <erik.saryan@tumo.org>
2025-01-21 11:40:38 +0400

    Fix paddings in main page also fixed some layouts

--- src/components/DescriptionProject/Description.jsx
+++ src/components/DescriptionProject/Description.jsx
@@ -25,7 +25,7 @@ const Description = () => {
                     <div data-aos="fade-up" className={`${styles.boxes}`}>
                         <h2 className={styles.title}>{t('description.sections.0.title')}</h2>
                         <p className={"mb-3"}> {t('description.sections.0.content')}</p>
-                        <div className={`d-flex mb-3`}>
+                        <div className={`d-flex mb-3 ${styles.partner_item}`}>
                             <p className={`${styles.partners} ${styles.partner_link}`}>
                                 <a href='https://aws.amazon.com/' >
                                     <img loading="lazy" className={`${styles.partner_logo}`} src={aws} alt={"AWS Logo"}/>
@@ -33,7 +33,7 @@ const Description = () => {
                             </p>
                             <p>{t('description.sections.0.partner1')}</p>
                         </div>
-                        <div className={`d-flex `}>
+                        <div className={`d-flex ${styles.partner_item} `}>
                             <p className={`${styles.partners} ${styles.partner_link}`}>
                                 <a href='https://armenia.tumo.org/'>
                                     <img  loading="lazy" className={`${styles.partner_logo}`} src={tumo} alt={"TUMO Logo"}/>
