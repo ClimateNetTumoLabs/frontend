65f2a4a4b533534a0027e96614a8295aa6560d87
Sona <sona.arzumanyan42@gmail.com>
2025-02-13 18:46:47 +0400

    payment section and cookiemanager removed from main branch and png to webp

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
     return (
         <div className="App">
-            <CookieManager/>
+            {/*<CookieManager/>*/}
             <Header/>
             <ScrollToTop/>
             <Routes>
